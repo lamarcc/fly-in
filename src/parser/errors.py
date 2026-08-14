d30c4a3240541759fe89dbb65e116a1fd6d6e088
clamarc <celamarc@student.42lyon.fr>
2026-08-14 05:06:59 +0200

    parsing finished soon

--- src/parser/errors.py
+++ src/parser/errors.py
@@ -17,42 +17,77 @@ class Error(Exception):
 
 
 class MapFileError(Exception):
-    def __init__(self, line, message):
+    def __init__(self, line):
         self.line = line
-        self.message = message
-        super().__init__(self.message)
 
     def __str__(self):
-        error = bcolors.FAIL + bcolors.BOLD + "[MapFileError]" + bcolors.ENDC
-        return error + bcolors.BOLD + f" Line {self.line}: {self.message}"
+        error_type = bcolors.FAIL + bcolors.BOLD + "[MapFileError] " + bcolors.ENDC
+        return error_type + bcolors.BOLD + f"Line {self.line}: "
+
+    def test(self):
+        self.t.append(self.__str__())
 
     def msg(line, message):
         error = bcolors.FAIL + bcolors.BOLD + "[MapFileError] " + bcolors.ENDC + bcolors.BOLD + f"Line {line}: "
         return error + message
 
     def warning(line, message):
-            warning = bcolors.WARNING + bcolors.BOLD + "[MapFileWarning] " + bcolors.ENDC + bcolors.BOLD + f"Line {line}: "
-            return warning + message
+        warning = bcolors.WARNING + bcolors.BOLD + "[MapFileWarning] " + bcolors.ENDC + bcolors.BOLD + f"Line {line}: "
+        return warning + message
 
 class HubError(MapFileError):
-    pass
+    def __init__(self, line, message):
+        self.message = message
+        super().__init__(line)
 
-
-class ConnectionError(MapFileError):
-    pass
-
-
-class InvalidKeyError(MapFileError):
-    pass
+    def __str__(self):
+        error = super().__str__()
+        return error + self.message
 
 
 class MetadataError(MapFileError):
-    pass
+    def __init__(self, line, key):
+        self.key = key
+        super().__init__(line)
+
+    def __str__(self):
+        error = super().__str__()
+        return error + f"Unknown metadata '{self.key}'"
+
+
+class InvalidKeyError(MapFileError):
+    def __init__(self, line, key):
+        self.key = key
+        super().__init__(line)
+
+    def __str__(self):
+        error = super().__str__()
+        return error + f"Invalid '{self.key}' map information"
 
 
 class InvalidLineError(MapFileError):
-    pass
+    def __init__(self, line):
+        super().__init__(line)
+
+    def __str__(self):
+        error = super().__str__()
+        return error + "Too much arguments"
 
 
 class InvalidDronesValue(MapFileError):
-    pass
+    def __init__(self, line, message):
+        self.message = message
+        super().__init__(line)
+
+    def __str__(self):
+        error = super().__str__()
+        return error + self.message
+
+
+class DoublonError(MapFileError):
+    def __init__(self, line, key):
+        super().__init__(line)
+
+    def __str__(self):
+        error = super().__str__()
+        return error + f"Already defined earlier '{self.key}'"
--- src/parser/parsing.py
+++ src/parser/parsing.py
@@ -20,28 +20,33 @@ class Parse():
     connection = []
     hub_name = []
     nb_line = 0
+    err = []
 
     @staticmethod
     def parse(file: str):
         try:
             f = open(file)
             for line in f:
-                Parse.nb_line += 1
-                if line.startswith("#"):
-                    continue
-                if line.startswith("\n") and len(line) == 1:
-                    continue
-                key, info = line.split(":", 1)
-                key = key.strip()
-                info = info.strip()
-                Parse.check_line(key, info)
-                Parse.parse_key_info(key, info)
-        except ValueError:
-            print(errors.MapFileError.msg(f"Line {Parse.nb_line}: No key found (':' missing)"))
+                try:
+                    Parse.nb_line += 1
+                    if line.startswith("#"):
+                        continue
+                    if line.startswith("\n") and len(line) == 1:
+                        continue
+                    key, info = line.split(":", 1)
+                    key = key.strip()
+                    info = info.strip()
+                    Parse.check_line(key, info)
+                    Parse.parse_key_info(key, info)
+                except ValueError:
+                    print(errors.MapFileError.msg("No key found (':' missing)"))
+                except errors.MapFileError as e:
+                    Parse.err.append(str(e))
+            if Parse.err:
+                for error in Parse.err:
+                    print(error)
         except FileNotFoundError:
-            print(errors.MapFileError.msg(f"File does not exist"))
-        except errors.MapFileError as e:
-            print(e)
+            print(errors.MapFileError.msg("File does not exist"))
 
     @staticmethod
     def check_line(key, info):
@@ -50,10 +55,10 @@ class Parse():
         if m_start != -1 and m_end != -1:
             tmp = info.replace(info[m_start:m_end + 1], "")
             if (key == "hub" and len(tmp.split()) > 3) or (key == "connection" and len(tmp.split()) > 1):
-                raise errors.InvalidLineError(Parse.nb_line, "too much arguments")
+                raise errors.InvalidLineError(Parse.nb_line)
         else:
             if (key == "hub" and len(info.split()) > 3) or (key == "connection" and len(info.split()) > 1):
-                raise errors.InvalidLineError(Parse.nb_line, "too much arguments")
+                raise errors.InvalidLineError(Parse.nb_line)
 
     @staticmethod
     def parse_key_info(key: str, info: str):
@@ -61,10 +66,10 @@ class Parse():
             try:
                 if Parse.nb_drones == 0:
                     if int(info) < 1:
-                        raise errors.InvalidDronesValue(Parse.nb_line, "cant have less than 1 drone")
+                        raise errors.InvalidDronesValue(Parse.nb_line, "Cant have less than 1 drone")
                     Parse.nb_drones = int(info)
                 else:
-                    raise errors.MapFileError(Parse.nb_line, "already defined earlier")
+                    raise errors.DoublonError(Parse.nb_line, key)
             except ValueError:
                 raise errors.InvalidDronesValue(Parse.nb_line, "'nb_drones' value need to be an integer")
         elif key == "start_hub":
@@ -72,37 +77,32 @@ class Parse():
                 Parse.start_hub = Parse.parse_hub(info)
                 Parse.try_position(Parse.start_hub)
                 Parse.hubs.append(Parse.start_hub)
-                Parse.hub_name.append(Parse.start_hub["name"])
             else:
-                raise errors.HubError(Parse.nb_line, "can't have 2 start_hub")
+                raise errors.HubError(Parse.nb_line, "Can't have 2 start_hub")
         elif key == "end_hub":
             if not Parse.end_hub:
                 Parse.end_hub = Parse.parse_hub(info)
                 Parse.try_position(Parse.end_hub)
                 Parse.hubs.append(Parse.end_hub)
-                Parse.hub_name.append(Parse.end_hub["name"])
             else:
-                raise errors.HubError(Parse.nb_line, "can't have 2 end_hub")
+                raise errors.HubError(Parse.nb_line, "Can't have 2 end_hub")
         elif key == "hub":
             tmp_hub = Parse.parse_hub(info)
             Parse.try_position(tmp_hub)
-            if tmp_hub["name"] in Parse.hub_name:
-                raise errors.HubError(Parse.nb_line, f"'{tmp_hub["name"]}' already defined earlier")
             Parse.hubs.append(tmp_hub)
-            Parse.hub_name.append(tmp_hub["name"])
         elif key == "connection":
             parsed_connection = Parse.parse_connection(info)
             for connection in Parse.connection:
                 if set(parsed_connection["connection"]) == set(connection["connection"]):
-                    raise errors.ConnectionError(Parse.nb_line, f"'{parsed_connection['connection']}' already defined earlier")
+                    raise errors.DoublonError(Parse.nb_line, parsed_connection['connection'])
             hub1, hub2 = parsed_connection["connection"]
             if hub1 not in Parse.hub_name:
-                raise errors.HubError(Parse.nb_line, f"'{hub1}' not defined yet")
+                raise errors.HubError(Parse.nb_line, f"Hub '{hub1}' not defined yet")
             if hub2 not in Parse.hub_name:
-                raise errors.HubError(Parse.nb_line, f"'{hub2}' not defined yet")
+                raise errors.HubError(Parse.nb_line, f"Hub '{hub2}' not defined yet")
             Parse.connection.append(parsed_connection)
         else:
-            raise errors.InvalidKeyError(Parse.nb_line, f"invalid '{key}' key")
+            raise errors.InvalidKeyError(Parse.nb_line, key)
 
     @staticmethod
     def try_position(hub_info):
@@ -110,7 +110,7 @@ class Parse():
             int(hub_info["pos_x"])
             int(hub_info["pos_y"])
         except ValueError:
-            raise errors.HubError(Parse.nb_line, f"undefined '{hub_info["name"]}' hub position")
+            raise errors.HubError(Parse.nb_line, f"Undefined '{hub_info["name"]}' hub position")
 
     @staticmethod
     def parse_hub(line: str):
@@ -122,11 +122,17 @@ class Parse():
         if metadata_index != -1:
             tmp_line = line[:metadata_index].strip()
             hub_info = {key: value for key, value in zip(["name", "pos_x", "pos_y"], tmp_line.split())}
+            if hub_info["name"] in Parse.hub_name:
+                raise errors.HubError(Parse.nb_line, f"'{hub_info["name"]}' already defined earlier")
+            Parse.hub_name.append(hub_info["name"])
             hub_info["metadata"] = Parse.parse_metadata(hub_info["name"], line[metadata_index:], default_metadata)
             return {**default_info, **hub_info}
         else:
             tmp_line = line.strip()
             hub_info = {key: value for key, value in zip(["name", "pos_x", "pos_y"], tmp_line.split())}
+            if hub_info["name"] in Parse.hub_name:
+                raise errors.HubError(Parse.nb_line, f"'{hub_info["name"]}' already defined earlier")
+            Parse.hub_name.append(hub_info["name"])
             hub_info["metadata"] = default_metadata
             return {**default_info, **hub_info}
 
@@ -137,17 +143,17 @@ class Parse():
         try:
             for key in new_metadata.keys():
                 if key not in ["zone", "color", "max_drones"]:
-                    raise errors.InvalidKeyError(Parse.nb_line, f"'{key}' not a valid key")
+                    raise errors.MetadataError(Parse.nb_line, key)
             if "zone" in new_metadata and new_metadata["zone"] not in ["normal", "blocked", "restricted", "priority"]:
-                raise errors.HubError(Parse.nb_line, "invalid zone_type")
+                raise errors.HubError(Parse.nb_line, "Invalid zone_type")
             if "color" in new_metadata and new_metadata["color"].lower() not in Color.list:
                 print(errors.MapFileError.warning(Parse.nb_line, f"Unknown color for hub '{name}', color set to grey by default"))
                 new_metadata["color"] = "grey"
             if "max_drones" in new_metadata:
                 int(new_metadata["max_drones"])
-            return new_metadata
         except ValueError:
             raise errors.InvalidDronesValue(Parse.nb_line, "invalid 'max_drones' value")
+        return new_metadata
 
     @staticmethod
     def parse_connection(line: str):
@@ -160,7 +166,7 @@ class Parse():
                 if check_metadata != "max_link_capacity":
                     invalid_data.append(check_metadata)
             if len(invalid_data):
-                raise errors.MetadataError(Parse.nb_line, f"'{invalid_data}' invalid metadata")
+                raise errors.MetadataError(Parse.nb_line, invalid_data)
             return {"connection": line[:metadata_index].strip().split("-"), "metadata": metadata}
         else:
             return {"connection": line.strip().split("-"), "metadata": default_metadata}
